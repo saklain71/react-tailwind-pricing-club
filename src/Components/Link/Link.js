2a210dce8b2f4be3182f2feda66ce32ada0fbaa6
Saklain Mustak <saklainmustak@sakmac.local>
2022-03-30 22:27:57 +0600

    menubar added

--- src/Components/Link/Link.js
+++ src/Components/Link/Link.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = (props) => {
     const {name , link} = props.route;
     return (
-        <li>
+        <li className='mr-16'>
             <a href={link}>{name}</a>
            
         </li>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import {MenuIcon, XIcon} from '@heroicons/react/solid';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     const routes = [
         { id: 1, name: 'Home', link:'/home'},
         { id: 2, name: 'Deals', link:'/deals'},
@@ -10,7 +12,10 @@ const Navbar = () => {
     ]
     return (
        <nav>
-           <ul>
+           <div onClick={()=> setOpen(!open)} className='w-6 h-6 md:hidden'>
+               {open ? <XIcon></XIcon> :<MenuIcon></MenuIcon>}
+           </div>
+           <ul className={`md:flex justify-center absolute duration-500 ease-in ${open ? 'top-6': 'top-[-120px]'}`}>
               {
                   routes.map(route => <Link
                   route = {route}
